07375aa4e863912347928c2e924202338568ad8c
Chen <chenyu.zhu86@gmail.com>
2018-12-13 18:38:58 -0500

    finished creating schema

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   if Rails.env.development?
+
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    
   end  
 
   post "/graphql", to: "graphql#execute"
